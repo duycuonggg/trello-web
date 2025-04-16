577918460696d205c913034fa9828f579767629f
duycuonggg <thivip201197@gmail.com>
2025-04-16 14:14:30 +0700

    app bar p1

---
+++ src/components/AppBar/Menus/Profile.jsx
@@ -0,0 +1,81 @@
+import React from 'react'
+import Menu from '@mui/material/Menu'
+import MenuItem from '@mui/material/MenuItem'
+import Box from '@mui/material/Box'
+import Divider from '@mui/material/Divider'
+import ListItemIcon from '@mui/material/ListItemIcon'
+import Avatar from '@mui/material/Avatar'
+import Tooltip from '@mui/material/Tooltip'
+import IconButton from '@mui/material/IconButton'
+import PersonAdd from '@mui/icons-material/PersonAdd'
+import Settings from '@mui/icons-material/Settings'
+import Logout from '@mui/icons-material/Logout'
+
+function Profile() {
+  const [anchorEl, setAnchorEl] = React.useState(null)
+  const open = Boolean(anchorEl)
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget)
+  }
+  const handleClose = () => {
+    setAnchorEl(null)
+  }
+  return (
+    <Box>
+      <Tooltip title="Account settings">
+        <IconButton
+          onClick={handleClick}
+          size="small"
+          sx={{ padding: 0 }}
+          aria-controls={open ? 'basic-menu-profile' : undefined}
+          aria-haspopup="true"
+          aria-expanded={open ? 'true' : undefined}
+        >
+          <Avatar
+            sx={{ width: 30, height: 30 }}
+            alt='Cường đẹp trai'
+            src='https://scontent-hkg1-1.xx.fbcdn.net/v/t39.30808-6/434676690_442145844943398_3149193981518289588_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=1dvUdTliDOsQ7kNvwGXM4VN&_nc_oc=AdmkdATQKV8Wr3Vnyn2Nq79SJAk-FahGlYYRhZcokMt2cMDFDed05eAAn-N1rLI1KNE&_nc_zt=23&_nc_ht=scontent-hkg1-1.xx&_nc_gid=BC7VHcCQTo0kKWCWQdf58w&oh=00_AfFUI0l2Eob72tPjPvzWyCZKvBVDVOP-1LCneZS-_WG9qA&oe=68051B35'
+          />
+        </IconButton>
+      </Tooltip>
+      <Menu
+        id="basic-menu-profile"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        MenuListProps={{
+          'aria-labelledby': 'basic-button-profile'
+
+        }}
+      >
+        <MenuItem>
+          <Avatar sx={{ width: 28, height: 28, mr: 2 }}/> Profile
+        </MenuItem>
+        <MenuItem>
+          <Avatar sx={{ width: 28, height: 28, mr: 2 }}/> My account
+        </MenuItem>
+        <Divider />
+        <MenuItem>
+          <ListItemIcon>
+            <PersonAdd fontSize="small" />
+          </ListItemIcon>
+          Add another account
+        </MenuItem>
+        <MenuItem>
+          <ListItemIcon>
+            <Settings fontSize="small" />
+          </ListItemIcon>
+          Settings
+        </MenuItem>
+        <MenuItem>
+          <ListItemIcon>
+            <Logout fontSize="small" />
+          </ListItemIcon>
+          Logout
+        </MenuItem>
+      </Menu>
+    </Box>
+  )
+}
+
+export default Profile
