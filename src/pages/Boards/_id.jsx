593a642be5fb1529c5c87a8b2334aa64eacd3993
duycuonggg <thivip201197@gmail.com>
2025-04-29 16:40:14 +0700

    mock sort data

--- src/pages/Boards/_id.jsx
+++ src/pages/Boards/_id.jsx
@@ -3,13 +3,15 @@ import Container from '@mui/material/Container'
 import AppBar from '~/components/AppBar/AppBar'
 import BoardBar from './BoardBar/Boardbar'
 import BoardContent from './BoardContent/BoardContent'
+import { mockData } from '~/apis/mock-data'
 
 function Board() {
   return (
     <Container disableGutters maxWidth={false} sx={{ height: '100vh' }}>
       <AppBar />
-      <BoardBar />
-      <BoardContent />
+      {/* ?. (optional chaining): thay cho if nếu có board thì in ra */}
+      <BoardBar board={mockData?.board} />
+      <BoardContent board={mockData?.board} />
     </Container>
   )
 }
