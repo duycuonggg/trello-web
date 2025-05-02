58f888db3bd268c9c3db99482e734ab0c3bf06cf
duycuonggg <thivip201197@gmail.com>
2025-05-02 16:14:07 +0700

    drag drop column

--- src/pages/Boards/BoardContent/ListColumns/Column/Column.jsx
+++ src/pages/Boards/BoardContent/ListColumns/Column/Column.jsx
@@ -20,6 +20,8 @@ import AddcardIcon from '@mui/icons-material/AddCard'
 import DragHandleIcon from '@mui/icons-material/DragHandle'
 import ListCards from './ListCards/ListCards'
 import { mapOrder } from '~/utils/sorts'
+import { useSortable } from '@dnd-kit/sortable'
+import { CSS } from '@dnd-kit/utilities'
 
 function Column({ column }) {
   const [anchorEl, setAnchorEl] = useState(null)
@@ -31,19 +33,38 @@ function Column({ column }) {
     setAnchorEl(null)
   }
 
+  // drag drop
+  const {
+    attributes, listeners, setNodeRef, transform, transition } = useSortable({
+      id: column._id,
+      data: { ...column }
+    })
+  const dndKitColumnStyles = {
+    // dành cho sensor defaut dạng pointerSensor
+    // touchAction: 'none',
+    // nếu sử dụng Transform sẽ bị stretch
+    transform: CSS.Translate.toString(transform),
+    transition
+  }
+
   const orderedCards = mapOrder(column?.cards, column?.cardOrderIds, '_id')
   return (
-    < Box sx={{
-      minWidth: '300px',
-      maxWidth: '300px',
-      bgcolor: (theme) => (theme.palette.mode === 'dark' ? '#333643' : '#ebecf0'),
-      // margin left
-      ml: 2,
-      borderRadius: '6px',
-      height: 'fit-content',
-      maxHeight: (theme) => `calc(${theme.trello.boardContentHeight} - ${theme.spacing(5)})`
-    }
-    }>
+    < Box
+      ref={setNodeRef}
+      style={dndKitColumnStyles}
+      {...attributes}
+      {...listeners}
+      sx={{
+        minWidth: '300px',
+        maxWidth: '300px',
+        bgcolor: (theme) => (theme.palette.mode === 'dark' ? '#333643' : '#ebecf0'),
+        // margin left
+        ml: 2,
+        borderRadius: '6px',
+        height: 'fit-content',
+        maxHeight: (theme) => `calc(${theme.trello.boardContentHeight} - ${theme.spacing(5)})`
+      }
+      }>
       {/* header */}
       < Box sx={{
         height: (theme) => (theme.trello.columnHeaderHeight),
