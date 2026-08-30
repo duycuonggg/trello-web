a0df356c248e8f2c95a63c0d5a9e137af9f282f1
duycuonggg <thivip201197@gmail.com>
2026-08-30 12:27:46 +0700

    settingUser_changeAvatar_password

--- src/redux/user/userSlice.js
+++ src/redux/user/userSlice.js
@@ -26,6 +26,14 @@ export const logoutUserAPI = createAsyncThunk(
   }
 )
 
+export const updateUserAPI = createAsyncThunk(
+  'user/updateUserAPI',
+  async (data) => {
+    const response = await authorizedAxiosInstance.put(`${API_ROOT}/v1/users/update`, data)
+    return response.data
+  }
+)
+
 export const userSlice = createSlice({
   name: 'user',
   initialState,
@@ -43,6 +51,10 @@ export const userSlice = createSlice({
        * Kết hợp ProtectedRoute đã làm ở App.js => code sẽ điều hướng chuẩn về trang Login
        */
       state.currentUser = null
+    })
+    builder.addCase(updateUserAPI.fulfilled, (state, action) => {
+      const user = action.payload
+      state.currentUser = user
     })
   }
 })
